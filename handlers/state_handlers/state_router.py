dfde3b54efea5ef6b4c5869a7d44433d0c7936f1
Glebastaa <gora_2000@mail.ru>
2024-05-11 19:55:12 +0500

    revork v4

--- handlers/state_handlers/state_router.py
+++ handlers/state_handlers/state_router.py
@@ -9,6 +9,5 @@ router = Router()
 
 router.include_router(studio.router)
 router.include_router(student.router)
-#router.include_router(schedule.router)
 router.include_router(group.router)
 #router.include_router(user.router)
